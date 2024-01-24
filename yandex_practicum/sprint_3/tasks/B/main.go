16acfda7db90f9dfc04c7d32bd57877aa22cf53d
cyriljupm <cyriljupm@gmail.com>
2024-01-24 23:42:30 +0300

    sprint_3 task B

---
+++ yandex_practicum/sprint_3/tasks/B/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var letters = map[int]string{
+	2: "abc",
+	3: "def",
+	4: "ghi",
+	5: "jkl",
+	6: "mno",
+	7: "pqrs",
+	8: "tuv",
+	9: "wxyz",
+}
+
+func main() {
+	reader := makeReader()
+	writer := makeWriter()
+
+	digits := readArr(reader)
+
+	solution(digits, "", writer)
+
+	writer.Flush()
+}
+
+func solution(digits []int, prefix string, writer *bufio.Writer) {
+	if len(digits) == 0 {
+		writer.WriteString(prefix + " ")
+		return
+	}
+
+	digit := digits[0]
+	for _, letter := range letters[digit] {
+		solution(digits[1:], prefix+string(letter), writer)
+	}
+}
+
+func makeReader() *bufio.Reader {
+	reader := bufio.NewReader(os.Stdin)
+
+	return reader
+}
+
+func readArr(reader *bufio.Reader) []int {
+	str, _ := reader.ReadString('\n')
+	arrStr := strings.Split(strings.TrimSpace(str), "")
+	arrInt := make([]int, len(arrStr))
+
+	for i, val := range arrStr {
+		arrInt[i], _ = strconv.Atoi(val)
+	}
+
+	return arrInt
+}
+
+func makeWriter() *bufio.Writer {
+	writer := bufio.NewWriter(os.Stdout)
+
+	return writer
+}
